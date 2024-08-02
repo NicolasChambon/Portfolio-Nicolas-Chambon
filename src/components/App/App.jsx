8df5736c9baeca22052b543fe6d6ddd79372047e
NicolasChambon <nico.chambon@gmail.com>
2024-08-02 16:17:47 +0200

    home page desktop integration done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,10 +5,28 @@ import './App.scss';
 
 const App = () => {
   return (
-    <>
+    <div className="App">
       <Header />
-      <Home />
-    </>
+      <div className="App-background">
+        <Home />
+      </div>
+      <p className="App-credits">
+        Picture by{' '}
+        <a
+          className="App-credits-link"
+          href="https://unsplash.com/fr/@fiveamstories?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+        >
+          Alex Shutin
+        </a>{' '}
+        on{' '}
+        <a
+          className="App-credits-link"
+          href="https://unsplash.com/fr/photos/photographie-panoramique-de-montagnes-kKvQJ6rK6S4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+        >
+          Unsplash
+        </a>
+      </p>
+    </div>
   );
 };
 
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,9 +6,37 @@ const Home = () => {
   return (
     <main className="Home">
       <div className="Home-logo">
-        <img src={NcLogo} alt="Logo Portfolio NC" />
-        <h1 className="Home-logo-title">Fullstack Web Developer</h1>
+        <img className="Home-logo-svg" src={NcLogo} alt="Logo Portfolio NC" />
+        <div className="Home-logo-text">
+          <h1 className="Home-logo-text-title">Fullstack Web Developer</h1>
+          <h2 className="Home-logo-text-name">Nicolas Chambon</h2>
+        </div>
       </div>
+      <p className="Home-description">Hello and welcome to my portfolio !</p>
+      <p className="Home-description">
+        My name is Nicolas Chambon, and I am a passionate Fullstack Web
+        Developer. With a unique background in sport biomechanics and product
+        engineering. Today, I bring a diverse skill set and a dedication to
+        creating innovative and efficient web solutions.
+      </p>
+      <p className="Home-description">
+        I have a strong foundation in frontend technologies like React.js and
+        backend frameworks such as Express with Node.js, and I am always eager
+        to learn and adapt to new challenges.
+      </p>
+      <p className="Home-description">
+        This portfolio is a showcase of my journey and achievements in web
+        development. Here, you will find my detailed CV, highlighting my
+        technical skills. You can also explore a collection of my projects, each
+        accompanied by a brief description, demonstration videos, and links to
+        live sites and/or GitHub repositories.
+      </p>
+      <p className="Home-description">
+        Feel free to reach out to me if you have any questions or if you are
+        interested in collaborating on exciting web development projects. Thank
+        you for visiting, and I hope you enjoy browsing through my work !
+      </p>
+      <p className="Home-description last">Nicolas Chambon</p>
     </main>
   );
 };
